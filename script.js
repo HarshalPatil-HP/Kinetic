d8488139c39e449b7986b3a024b88c1ec7e0e6b6
HarshalPatil-HP <chandrkantpatil080@gmail.com>
2026-09-03 22:47:10 +0530

    Fix cursor animation calculation in script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let cursorY = 0;
 );
 
  function animateCursor() {
-   cursorX += (mouseX - cursorX) * 0.1;
+cursorX += (mouseX - cursorX) * 0.1;
 cursorY += (mouseY - cursorY) * 0.1;
 
   cursor.style.left = cursorX + 'px';
